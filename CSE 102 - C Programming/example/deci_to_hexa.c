121fa58b858b92bd65dfcb2ba2c98bb8b87e7392
ebylmz17 <ebylmz17@gmail.com>
2021-10-07 21:46:36 +0300

    implemented optimization

--- CSE 102 - C Programming/example/deci_to_hexa.c
+++ CSE 102 - C Programming/example/deci_to_hexa.c
@@ -3,23 +3,19 @@
 #include <time.h>
 
 char * deci_to_hexa (int deci) {
-    int r, i = 0, j;
     char tmp[20], * hexa;
+    int r, i = 0, j;
 
-    if (deci == 0)
-        tmp[i++] = '0';
-    else {
-        do {
-            r = deci % 16;
-            
-            if (r < 10)
-                tmp[i++] =  r + '0';
-            else
-                tmp[i++] = r - 10 + 'A';
-            
-            deci /= 16;
-        } while (deci > 0);
-    }
+    do {
+        r = deci % 16;
+        
+        if (r < 10)
+            tmp[i++] =  r + '0';
+        else
+            tmp[i++] = r - 10 + 'A';
+        
+        deci /= 16;
+    } while (deci > 0);
 
     hexa = (char *) calloc(i + 1, sizeof(char));
     if (hexa != NULL) {
